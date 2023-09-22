b95244ff1c77fe0b176f851506028cc2004b0de2
AndrésLabat <andreslabat89@gmail.com>
2023-09-23 00:53:02 +0200

    añadidos eventos a los botones A, B y HOME

--- Js/main.js
+++ Js/main.js
@@ -41,8 +41,8 @@ joyconIzquierdo.addEventListener("click", () => {
             joyconIzquierdo.style.marginLeft = "24.5em";   //49em
         }, 2000);
         desplazamientoJoyconIzquierdo = false;
-    }    
-});    
+    }
+});
 
 
 // CONSOLA Y DOC:
@@ -57,10 +57,10 @@ baseDeCarga.addEventListener("click", () => {
         consola.style.marginTop = "";
         setTimeout(() => {
             consola.style.marginRight = "";
-        }, 2000);    
+        }, 2000);
         setTimeout(() => {
             baseDeCarga.style.marginLeft = "";
-        }, 2000); 
+        }, 2000);
 
         margenesAplicados = true;
 
@@ -77,14 +77,14 @@ baseDeCarga.addEventListener("click", () => {
         }, 2000);
 
         desplazamientoJoyconIzquierdo = true;
-        
+
 
     } else {
         baseDeCarga.style.marginLeft = "35em";
         consola.style.marginRight = "35em";
         setTimeout(() => {
             consola.style.marginTop = "8em";
-        }, 2000);    
+        }, 2000);
 
         margenesAplicados = false;
 
@@ -101,8 +101,8 @@ baseDeCarga.addEventListener("click", () => {
         }, 2000);
 
         desplazamientoJoyconIzquierdo = false;
-    }    
-});    
+    }
+});
 
 
 // BOTON ENCENDIDO:
@@ -110,28 +110,80 @@ baseDeCarga.addEventListener("click", () => {
 const botonOn = document.querySelector("#botonEncendido");
 const luzEncendido = document.querySelector(".luzEncendido");
 const pantalla = document.querySelector(".pantalla");
+let imagen = 0;
 
 let botonOnEncendido = false;
 
 botonOn.addEventListener("click", () => {
     if (botonOnEncendido == false) {
-        pantalla.style.backgroundImage  = "";
+        pantalla.style.backgroundImage = "";
+        pantalla.style.filter = "grayscale(100%)";
         luzEncendido.style.backgroundColor = "#303235"
         botonOnEncendido = true;
     } else {
-        pantalla.style.backgroundImage = "url('video\video de fondo.mp4')";
-        luzEncendido.style.backgroundColor = "#D6FF99"
+        pantalla.style.backgroundImage = "url('img/menu nsw.jpg')";
+        pantalla.style.backgroundSize = "cover";
+        pantalla.style.filter = "none";
+        luzEncendido.style.backgroundColor = "#D6FF99";
         botonOnEncendido = false;
-    }    
-});    
+        let imagen = 0;
+    }
+});
 
 
 
 // BOTON HACIA DELANTE:
 
-const botonA = document.getElementById("botonA");
+const botonA = document.querySelector("#botonA");
+
+botonA.addEventListener("click", () => {
+    event.stopPropagation();
+    if (botonOnEncendido == false && pantalla.style.backgroundImage === 'url("img/menu nsw.jpg")') {
+        pantalla.style.backgroundImage = "url('img/super mario odyssey.jpg')";
+        pantalla.style.backgroundSize = "cover";
+        imagen = 1;
+    }
+    else if (imagen === 1) {
+        pantalla.style.backgroundImage = "url('img/super mario odyssey in game.jpg')";
+        pantalla.style.backgroundSize = "cover";
+        imagen = 2;
+    }
+});
 
 
 // BOTON HACIA DETRAS:
 
 const botonB = document.getElementById("botonB");
+
+botonB.addEventListener("click", () => {
+    event.stopPropagation();
+    if (imagen === 1) {
+        pantalla.style.backgroundImage = "url('img/menu nsw.jpg')";
+        pantalla.style.backgroundSize = "cover";
+        imagen = 0;
+    }
+    else if (imagen === 2) {
+        pantalla.style.backgroundImage = "url('img/super mario odyssey.jpg')";
+        pantalla.style.backgroundSize = "cover";
+        imagen = 1;
+    }
+});
+
+
+// BOTON DE RESETEO (HOME):
+
+const home = document.querySelector(".divBotonHome");
+
+home.addEventListener("click", () => {
+    event.stopPropagation();
+    if (imagen === 1) {
+        pantalla.style.backgroundImage = "url('img/menu nsw.jpg')";
+        pantalla.style.backgroundSize = "cover";
+        imagen = 0;
+    }
+    else if (imagen === 2) {
+        pantalla.style.backgroundImage = "url('img/menu nsw.jpg')";
+        pantalla.style.backgroundSize = "cover";
+        imagen = 0;
+    }
+});
