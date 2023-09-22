bfced248327fc18788abdf579c8cba75e7acaaa6
AndrésLabat <andreslabat89@gmail.com>
2023-09-22 21:05:20 +0200

    implementado cambio de color en luz de encendido al apretar al boton ON

--- Js/main.js
+++ Js/main.js
@@ -13,16 +13,16 @@ joyconDerecho.addEventListener("click", () => {
         }, 2000);
         desplazamientoJoyconDerecho = true;
     } else {
-        joyconDerecho.style.marginBottom = "34em";
+        joyconDerecho.style.marginBottom = "32em";
         setTimeout(() => {
-            joyconDerecho.style.marginRight = "49em";
+            joyconDerecho.style.marginRight = "24.5em";   //49em
         }, 2000);
         desplazamientoJoyconDerecho = false;
     }
 });
 
-// JOYCON IZQUIERDO:
 
+// JOYCON IZQUIERDO:
 
 const joyconIzquierdo = document.querySelector(".joyconIzquierdo");
 
@@ -30,16 +30,22 @@ let desplazamientoJoyconIzquierdo = false;
 
 joyconIzquierdo.addEventListener("click", () => {
     if (desplazamientoJoyconIzquierdo == false) {
-        joyconIzquierdo.style.marginBottom = "";
-        joyconIzquierdo.style.marginRight = "";
+        joyconIzquierdo.style.marginLeft = "";
+        setTimeout(() => {
+            joyconIzquierdo.style.marginBottom = "";
+        }, 2000);
         desplazamientoJoyconIzquierdo = true;
     } else {
-        joyconIzquierdo.style.marginBottom = "34em";
+        joyconIzquierdo.style.marginBottom = "32em";
+        setTimeout(() => {
+            joyconIzquierdo.style.marginLeft = "24.5em";   //49em
+        }, 2000);
         desplazamientoJoyconIzquierdo = false;
     }    
 });    
 
-// PANTALLA Y DOC:
+
+// CONSOLA Y DOC:
 
 const baseDeCarga = document.querySelector(".divDoc");
 const consola = document.querySelector(".consola");
@@ -55,18 +61,23 @@ baseDeCarga.addEventListener("click", () => {
         setTimeout(() => {
             baseDeCarga.style.marginLeft = "";
         }, 2000); 
+
+        margenesAplicados = true;
+
         joyconDerecho.style.marginRight = "";
         setTimeout(() => {
             joyconDerecho.style.marginBottom = "";
         }, 2000);
+
         desplazamientoJoyconDerecho = true;
 
-        joyconIzquierdo.style.marginBottom = "";
-        joyconIzquierdo.style.marginRight = "";
+        joyconIzquierdo.style.marginLeft = "";
+        setTimeout(() => {
+            joyconIzquierdo.style.marginBottom = "";
+        }, 2000);
 
         desplazamientoJoyconIzquierdo = true;
         
-        margenesAplicados = true;
 
     } else {
         baseDeCarga.style.marginLeft = "35em";
@@ -75,41 +86,52 @@ baseDeCarga.addEventListener("click", () => {
             consola.style.marginTop = "8em";
         }, 2000);    
 
-        joyconDerecho.style.marginBottom = "34em";
+        margenesAplicados = false;
+
+        joyconDerecho.style.marginBottom = "32em";
         setTimeout(() => {
-            joyconDerecho.style.marginRight = "49em";
+            joyconDerecho.style.marginRight = "24.5em";   //49em
         }, 2000);
 
         desplazamientoJoyconDerecho = false;
 
-        joyconIzquierdo.style.marginBottom = "34em";
-        
+        joyconIzquierdo.style.marginBottom = "32em";
+        setTimeout(() => {
+            joyconIzquierdo.style.marginLeft = "24.5em";   //49em
+        }, 2000);
+
         desplazamientoJoyconIzquierdo = false;
+    }    
+});    
 
-        margenesAplicados = false;
 
+// BOTON ENCENDIDO:
+
+const botonOn = document.querySelector("#botonEncendido");
+const luzEncendido = document.querySelector(".luzEncendido");
+const pantalla = document.querySelector(".pantalla");
+
+let botonOnEncendido = false;
+
+botonOn.addEventListener("click", () => {
+    if (botonOnEncendido == false) {
+        pantalla.style.backgroundImage  = "";
+        luzEncendido.style.backgroundColor = "#303235"
+        botonOnEncendido = true;
+    } else {
+        pantalla.style.backgroundImage = "url('video\video de fondo.mp4')";
+        luzEncendido.style.backgroundColor = "#D6FF99"
+        botonOnEncendido = false;
     }    
 });    
 
 
 
-/*-------------------------------------------------------------------------------*/
-
-const botonEncendido = document.getElementById("botonEncendido");
-const luzEncendido = document.getElementsByClassName("luzEncendido");
-const pantalla = document.getElementsByClassName("pantalla");
-
-botonEncendido.addEventListener("click", cambiarBackground);
-
-function cambiarBackground() {
-    if (articleEfectivo.style.backgroundColor === "rgb(127, 255, 174)") {
-        articleEfectivo.style.backgroundColor = "#f8f8f8";
-    } else {
-        articleEfectivo.style.backgroundColor = "rgb(127, 255, 174)";
-    }
-}
-
-/*-------------------------------------------------------------------------------*/
+// BOTON HACIA DELANTE:
 
 const botonA = document.getElementById("botonA");
+
+
+// BOTON HACIA DETRAS:
+
 const botonB = document.getElementById("botonB");
